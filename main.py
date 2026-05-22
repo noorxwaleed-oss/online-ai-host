af8b7865a27275097a51cd61d3eb854c6d1f0108
Noor <noorxwaleed22@gmail.com>
2026-05-22 13:53:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import json
 from cover_agent import CoverArtAgent
 
 
-
+with open("", "r", encoding="utf-8") as f:
+    metadata = json.load(f)
 
 # =========================
 # GENERATE IMAGE
@@ -11,10 +12,10 @@ from cover_agent import CoverArtAgent
 
 agent = CoverArtAgent()
 
-image = agent.invoke(
-    analyzer_output=metadata,
-    style="Minimalist",
+image = agent.generate(
+    metadata=metadata,
+    style="Colorful",
     aspect_ratio="2:3"
 )
 
-image
+image.show()
